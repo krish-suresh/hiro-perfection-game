4132dd5cadaa88efa39761d53ee363b3c43795c6
Krishna Suresh <krishna4132@gmail.com>
2021-12-13 20:06:04 -0500

    untested fix for movements

--- ros_code/MovingPieces.py
+++ ros_code/MovingPieces.py
@@ -27,9 +27,10 @@ class MovingPieces:
         self.robot.right_gripper.open()
         rospy.loginfo("Going home")
         self.robot.right_manipulator.home()
-    def move_to(self, pose_goal):
+    def move_to(self, pose_goal, reset_orientation=True):
         pose_goal.position.z +=0
-        pose_goal.orientation = self.default_gripper_quaternion
+        if reset_orientation:
+            pose_goal.orientation = self.default_gripper_quaternion
         self.robot.right_manipulator.set_pose_goal(pose_goal)
     def move_object(self,i):
         
@@ -53,21 +54,18 @@ class MovingPieces:
         print("Pick Pose: {}".format(top_right_corner_square))
         pose = Pose(position=Point(*top_right_corner_square))
         return pose
+    def place_square_piece_pose(self, index):
+        top_right_corner_square = [0.66, 0.07, 0.4]
+        top_right_corner_square[0] = top_right_corner_square[0]-0.03*(index/5)
+        top_right_corner_square[1] = top_right_corner_square[1]-0.03*(index%5)
+        print("Place Pose: {}".format(top_right_corner_square))
+        pose = Pose(position=Point(*top_right_corner_square), orientation=self.default_gripper_quaternion)
+        return pose 
     def identify_shape(self):
-        
         ret, frame = self.cam.read()
         predicted_shape = classify_shape.classify(frame)
         self.img_list.pop(predicted_shape[.0])
         return predicted_shape
-    def pick_final_square_pose(self, index):
-        bottom_right_corner_square = [0.7, -0.3, 0.4]
-        list_points = []
-        for i in range(0,5):
-            for j in range(0,5):
-                list_points.append([0.7+0.035*i, -0.3+0.035*j, 0.4])
-        transform_point = list_points[index]
-        pose = Pose(position=Point(*transform_point))
-        return pose
     def move_to_cv(self):
         transform_point = [0.55, -0.15, 0.4]
         pose_goal  = Pose(position = Point(*transform_point))
@@ -78,13 +76,8 @@ class MovingPieces:
         self.robot.right_gripper.set_value(90)
         pose = self.robot.right_manipulator.get_current_pose()
         print(pose)
-    def move_to_board_corner(self):
-        transform_point = [0.66, 0.07, 0.4]
-        pose = Pose(position=Point(*transform_point))
-        self.move_to(pose)
-        pose.position.z = self.place_height
-        self.move_to(pose)
     def pick_piece(self, i):
+        project.robot.right_gripper.set_value(90)
         pose = project.pick_square_piece_pose(i)
         rospy.loginfo("Grabbing Object Number #" + str(i))
         pose.position.z = 0.4
@@ -94,14 +87,25 @@ class MovingPieces:
         project.robot.right_gripper.close()
         pose.position.z = 0.4
         self.move_to(pose)
+    def place_piece(self, piece):
+        i = piece[0] # Piece number
+        pose = project.place_square_piece_pose(i)
+        rospy.loginfo("Placing Object Number #{} at {} deg".format(str(i), str(piece[1])))
+        pose.position.z = 0.4
+        pose.orientation = Quaternion(*quaternion_from_euler(pi, 0, piece[1])) # TODO check if rotation is right
+        self.move_to(pose, reset_orientation=False)
+        pose.position.z = self.place_height
+        self.move_to(pose, reset_orientation=False)
+        project.robot.right_gripper.set_value(90)
+        pose.position.z = 0.4
+        self.move_to(pose)
 if __name__ == '__main__':
     project = MovingPieces()
-    #project.run(2)
-    #project.test()
-    for i in range(25):
-        project.robot.right_gripper.set_value(90)
-        project.pick_piece(i)
-        project.move_to_cv()
-        project.move_to_board_corner()
-        project.robot.right_gripper.set_value(90)
+    # for i in range(25):
+    #     project.pick_piece(i)
+    #     project.move_to_cv()
+    #     piece = project.identify_shape()
+    #     project.place_piece(piece)
+
+    project.place_piece((0,pi/4)) # Test placing piece
     # project.current_pos()
